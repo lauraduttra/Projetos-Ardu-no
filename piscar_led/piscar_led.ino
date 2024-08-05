1c3477a8fa9c128b9cc7bde240fda8064d54b987
Laurinha <lauraduttra1@gmail.com>
2024-08-05 15:45:15 -0300

    inserido comentarios

--- piscar_led/piscar_led.ino
+++ piscar_led/piscar_led.ino
@@ -1,7 +1,8 @@
-void setup() {
+   //programa que envia sinal auto para a saída 7, aguarda um segundo, e envia sinal baixo para a saída 7
+  void setup (){
   // put your setup code here, to run once:
   pinMode (7,OUTPUT);
-  
+  //porta configurada com saída
 }
 
 void loop() {
